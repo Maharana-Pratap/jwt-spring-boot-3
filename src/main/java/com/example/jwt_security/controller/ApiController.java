aa7548e848945e7773b7f17abcbb7c2a43df802e
Maharana <45598520+Maharana-Pratap@users.noreply.github.com>
2025-09-16 00:37:21 +0530

    remove blank space

--- src/main/java/com/example/jwt_security/controller/ApiController.java
+++ src/main/java/com/example/jwt_security/controller/ApiController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
     @GetMapping("/test")
     public String test() {
         return "Hello Maharana";
